6c1d39a1749862cdaee725db62d20c03fa20b4fc
Angelos <angelos.naoumis@gmail.com>
2025-03-14 20:52:45 +0200

    Update AtlasLoot.lua

--- ElvUI_AngelosSkins/Skins/Vanilla/AtlasLoot.lua
+++ ElvUI_AngelosSkins/Skins/Vanilla/AtlasLoot.lua
@@ -3,32 +3,69 @@ local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
 local _G = _G
-local pairs = pairs
 
-local function AtlasLoot()
+function S:AtlasLootClassic()
 	if not E.db.AngelosSkins.AtlasLoot then return end
 
-    local ATL = _G["AtlasLoot_GUI-Frame"]
-    if not ATL or ATL.IsSkinned then return end
-    ATL.IsSkinned = true -- Prevent double skinning
+	local function SkinOnFrameShow()
 
-    -- Skin main frame & scrollbar
-    S:HandleFrame(ATL)
+		S:HandleFrame(_G["AtlasLoot_GUI-Frame"])
+		S:HandleFrame(_G["AtlasLoot-Select-1"])
+		S:HandleFrame(_G["AtlasLoot-Select-2"])
+		S:HandleFrame(_G["AtlasLoot-Select-3"])
+		S:HandleFrame(_G["AtlasLoot_GUI-Frame"].titleFrame)
+
+		S:HandleEditBox(_G["AtlasLoot_GUI-ItemFrame-SearchBox"])
+
+		S:HandleButton(_G["AtlasLoot-Button-1"])
+		S:HandleButton(_G["AtlasLoot-Button-2"])
+		S:HandleButton(_G["AtlasLoot-Button-3"])
+
+		S:HandleDropDown(_G["AtlasLoot-DropDown-1"])
+		S:HandleDropDown(_G["AtlasLoot-DropDown-2"])
+
+		S:HandleNextPrevButton(_G["AtlasLoot_GUI-ItemFrame-nextPageButton"])
+		S:HandleNextPrevButton(_G["AtlasLoot_GUI-ItemFrame-prevPageButton"])
+
+		-- Adjust Sizes
+		_G["AtlasLoot_GUI-ItemFrame-SearchBox"]:SetHeigth(_G["AtlasLoot_GUI-ItemFrame-SearchBox"]:GetHeigth-14)
+
+		-- Additional Things
+		-- Stip Dropdown Game Buttons
+		_G["AtlasLoot-DropDown-1-button"]:StripTextures()
+		_G["AtlasLoot-DropDown-1-button"]:SetTemplate('Transparent')
+		_G["AtlasLoot-DropDown-1-button"]:SetBackdrop(nil)
+		_G["AtlasLoot-DropDown-2-button"]:StripTextures()
+		_G["AtlasLoot-DropDown-2-button"]:SetTemplate('Transparent')
+		_G["AtlasLoot-DropDown-2-button"]:SetBackdrop(nil)
+
+		-- GameVersionButton/Logo
+		S:HandleButton(_G["AtlasLoot_GUI-Frame-gameVersionButton"])
+		_G["AtlasLoot_GUI-Frame-gameVersionButton"]:SetBackdrop(nil)
+		local transparentTexture = _G["AtlasLoot_GUI-Frame-gameVersionButton"]:CreateTexture(nil, "ARTWORK")
+		transparentTexture:SetAlpha(0)  -- Set the texture to be fully transparent
+
+		_G["AtlasLoot_GUI-Frame-gameVersionButton"]:SetHighlightTexture(transparentTexture)
+		_G["AtlasLoot_GUI-Frame-gameVersionButton"]:SetNormalTexture(538639)
+		_G["AtlasLoot_GUI-Frame-gameVersionButton"]:SetPushedTexture(538639)
+		_G["AtlasLoot_GUI-Frame-gameVersionLogo"]:Hide()  -- Hide the texture
+
+		-- Clear Extra lines on GameVersionButton
+		if _G["AtlasLoot_GUI-Frame"].gameVersionButton.Box then
+			for i, line in ipairs(_G["AtlasLoot_GUI-Frame"].gameVersionButton.Box) do
+				line:ClearAllPoints()  -- Remove any points the lines have
+				line:SetParent(nil)    -- Remove lines from their parent
+			end
+			_G["AtlasLoot_GUI-Frame"].gameVersionButton.Box = {}  -- Clear the Box table
+		end
+
+		-- Add backdrop to item frame
+		_G["AtlasLoot_GUI-ItemFrame"]:CreateBackdrop()
+
+		S:Unhook(_G["AtlasLoot_GUI-Frame"], "OnShow")
+	end
+
+	S:SecureHookScript(_G["AtlasLoot_GUI-Frame"], "OnShow", SkinOnFrameShow)
 end
 
-local function OnEvent(self, event, addon)
-    if event == "ADDON_LOADED" and addon == "SimpleAddonManager" then
-        local ATL = _G["AtlasLoot_GUI-Frame"]
-        if ATL then
-            if ATL:IsShown() then
-                AtlasLoot() -- Skin immediately if already visible
-            else
-                ATL:HookScript("OnShow", AtlasLoot)
-            end
-        end
-    end
-end
-
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("ADDON_LOADED")
-frame:SetScript("OnEvent", OnEvent)
+S:AddCallbackForAddon("AtlasLootClassic")
